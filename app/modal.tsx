6852c6b00623120aae7c50213caf8e6c37f89ff8
Kallum Burgin <Kallb123@users.noreply.github.com>
2024-08-09 23:26:50 +0100

    Initial expo app commit with directory picker

---
+++ app/modal.tsx
@@ -0,0 +1,66 @@
+import { View, Platform, Button } from 'react-native';
+import { Link, router } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as ScopedStorage from "react-native-scoped-storage"
+import { useCallback, useEffect, useState } from 'react';
+import { ThemedText } from '@/components/ThemedText';
+
+const DIRECTORY_KEY = "directory";
+
+export default function Modal() {
+    // If the page was reloaded or navigated to directly, then the modal should be presented as
+    // a full screen page. You may need to change the UI to account for this.
+    const isPresented = router.canGoBack();
+
+    const [directory, setDirectory] = useState("Unset");
+
+    const checkDirectory = useCallback(async () => {
+        let dir = "";
+        try {
+            dir = await AsyncStorage.getItem(DIRECTORY_KEY);
+            setDirectory(dir);
+        } catch (eRead) {
+            console.error(eRead);
+        }
+        if (!dir) {
+            console.log("No directory set, asking for a new one");
+            await selectNewDirectory();
+        }
+    }, []);
+
+    const selectNewDirectory = useCallback(async () => {
+        let selectedDir;
+        try {
+            selectedDir = await ScopedStorage.openDocumentTree(true);
+        } catch (eNew) {
+            // Toast to say selection cancelled?
+            return;
+        }
+        setDirectory(selectedDir.uri);
+        try {
+            await AsyncStorage.setItem(DIRECTORY_KEY, selectedDir.uri);
+        } catch (eWrite) {
+            console.error(eWrite);
+            // saving error
+        }
+    }, []);
+
+    useEffect(() => {
+        checkDirectory();
+    }, [checkDirectory]);
+
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            {/* Use `../` as a simple way to navigate to the root. This is not analogous to "goBack". */}
+            {!isPresented && <Link href="../">Dismiss</Link>}
+            {/* Native modals have dark backgrounds on iOS. Set the status bar to light content and add a fallback for other platforms with auto. */}
+            <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+            <ThemedText>Directory is: {directory}</ThemedText>
+            <Button
+                title="Change Directory"
+                onPress={selectNewDirectory}
+            />
+        </View>
+    );
+}
