bc8b65d126ec70061fa19f5afe3dc4716e05ebc0
Kallum Burgin <Kallb123@users.noreply.github.com>
2024-08-15 23:05:21 +0100

    Functioning settings page

--- app/settings.tsx
+++ app/settings.tsx
@@ -1,38 +1,97 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { Button, StyleSheet, TextInput } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { router } from 'expo-router';
 import * as ScopedStorage from "react-native-scoped-storage";
 import { useDispatch, useSelector } from 'react-redux';
-import { selectDirectory, selectPassword, setDirectory, setPassword } from '@/store/settingsReducer';
+import { contentTypes, dataSources, selectDataSource, selectDirectory, selectMediaStructure, selectMediaType, selectPassword, selectViewOrientation, selectViewScale, setDataSource, setDirectory, setMediaStructure, setMediaType, setPassword, setViewOrientation, setViewScale, viewOrientations, viewTypes } from '@/store/settingsReducer';
+import SelectDropdown from 'react-native-select-dropdown';
+import Slider from '@react-native-community/slider';
 
 export default function SettingsPrompt() {
-  const [dir, setDir] = useState(null as string | null);
+  const [directory, setLocalDirectory] = useState(null as string | null);
   const [password, setLocalPassword] = useState(null as string | null);
+  const [mediaType, setLocalMediaType] = useState("" as contentTypes);
+  const [dataSource, setLocalDataSource] = useState("" as dataSources);
+  const [mediaStructure, setLocalMediaStructure] = useState("" as viewTypes);
+  const [structureDescription, setStructureDescription] = useState("");
+  const [viewOrientation, setLocalViewOrientation] = useState("" as viewOrientations);
+  const [viewScale, setLocalViewScale] = useState(2);
 
   const dispatch = useDispatch();
   const settingsPassword = useSelector(selectPassword);
-  const directory = useSelector(selectDirectory);
+  const settingsDirectory = useSelector(selectDirectory);
+  const settingsMediaType = useSelector(selectMediaType);
+  const settingsDataSource = useSelector(selectDataSource);
+  const settingsMediaStructure = useSelector(selectMediaStructure);
+  const settingsViewOrientation = useSelector(selectViewOrientation);
+  const settingsViewScale = useSelector(selectViewScale);
+  
+  const mediaTypeRef = useRef(null);
+  const dataSourceRef = useRef(null);
+  const mediaStructureRef = useRef(null);
+  const viewOrientationRef = useRef(null);
+
+  const mediaTypeOptions = [
+    {id: 'tv', label: 'TV'},
+    {id: 'movies', label: 'Movies'},
+  ];
+
+  const dataSourceOptions = [
+    {id: 'tvdb', label: 'TVDB'},
+    // {id: 'imdb', label: 'IMDB'},
+    // {id: 'moviedb', label: 'TheMovieDB'},
+  ];
+
+  const viewTypeOptions = [
+    {id: 'flat', label: 'Flat', title: 'All episodes of all shows visible in one list'},
+    {id: 'show', label: 'Show', title: 'A folder for each show, with all episodes of that show then visible in each folder'},
+    {id: 'showplusseason', label: 'Show + Season', title: 'A folder for each show and season, with the episodes of that season in the folder'},
+    {id: 'showslashseason', label: 'Show/Season', title: 'A folder for each show, with a further folder for each season, with episodes then visible within'},
+  ];
+  
+  const uiTypeOptions = [
+    {id: 'poster', label: 'Poster'},
+    {id: 'banner', label: 'Banner'},
+  ];
   
   useEffect(() => {
-    console.log(`Got dir: ${directory} and pass: ${settingsPassword}`);
-    setDir(directory as string);
+    console.log(`Got dir: ${settingsDirectory}, pass: ${settingsPassword}, media: ${settingsMediaType}, dataSource: ${settingsDataSource}, mediaStructure: ${settingsMediaStructure}, orientation: ${settingsViewOrientation}, scale: ${settingsViewScale}`);
+    setLocalDirectory(settingsDirectory as string);
     setLocalPassword(settingsPassword as string);
-  }, [directory, settingsPassword]);
+    if (mediaTypeRef.current) (mediaTypeRef.current as any).selectIndex(mediaTypeOptions.findIndex(o => o.id === settingsMediaType));
+    if (dataSourceRef.current) (dataSourceRef.current as any).selectIndex(dataSourceOptions.findIndex(o => o.id === settingsDataSource));
+    if (mediaStructureRef.current) (mediaStructureRef.current as any).selectIndex(viewTypeOptions.findIndex(o => o.id === settingsMediaStructure));
+    if (viewOrientationRef.current) (viewOrientationRef.current as any).selectIndex(uiTypeOptions.findIndex(o => o.id === settingsViewOrientation));
+
+    setStructureDescription(viewTypeOptions.find(o => o.id === settingsMediaStructure)?.title ?? "");
+  }, [settingsDirectory, settingsPassword, settingsMediaType, settingsDataSource, settingsMediaStructure, settingsViewOrientation, settingsViewScale]);
 
   const save = () => {
-    console.log(dir);
-    if (dir) {
-      console.log(`Setting directory to: ${dir}`);
-      dispatch(setDirectory(dir));
+    console.log(`Saving dir: ${directory}, pass: ${password}, media: ${mediaType}, dataSource: ${dataSource}, mediaStructure: ${mediaStructure}, orientation: ${viewOrientation}, scale: ${viewScale}`);
+    if (directory) {
+      dispatch(setDirectory(directory));
     }
-    console.log(password);
     if (password) {
-      console.log(`Setting password to: ${password}`);
       dispatch(setPassword(password));
+    }
+    if (mediaType) {
+      dispatch(setMediaType(mediaType));
+    }
+    if (dataSource) {
+      dispatch(setDataSource(dataSource));
+    }
+    if (mediaStructure) {
+      dispatch(setMediaStructure(mediaStructure));
+    }
+    if (viewOrientation) {
+      dispatch(setViewOrientation(viewOrientation));
+    }
+    if (viewScale) {
+      dispatch(setViewScale(viewScale));
     }
     router.replace('/(drawer)')
   };
@@ -45,8 +104,12 @@ export default function SettingsPrompt() {
         // Toast to say selection cancelled?
         return;
     }
-    setDir(selectedDir.uri);
+    setLocalDirectory(selectedDir.uri);
   }, []);
+
+  const handleUIScaleChange = (value: number) => {
+    setLocalViewScale(11-value);
+  }
   
   return (
     <ParallaxScrollView
@@ -73,7 +136,144 @@ export default function SettingsPrompt() {
             onPress={selectNewDirectory}
         />
       </ThemedView>
-      <ThemedText>Directory is: {dir}</ThemedText>
+      <ThemedText>Directory is: {directory}</ThemedText>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText>Media type:</ThemedText>
+        <SelectDropdown
+          ref={mediaTypeRef}
+          data={mediaTypeOptions}
+          defaultValue={settingsMediaType}
+          onSelect={(selectedItem, index) => {
+            setLocalMediaType(selectedItem.id);
+          }}
+          renderButton={(selectedItem, isOpened) => {
+            return (
+              <View style={styles.dropdownButtonStyle}>
+                <Text style={styles.dropdownButtonTxtStyle}>
+                  {(selectedItem && selectedItem.label) || 'Please select...'}
+                </Text>
+                <Text>{isOpened ? "🔼" : "🔽"}</Text>
+              </View>
+            );
+          }}
+          renderItem={(item, index, isSelected) => {
+            return (
+              <View style={{...styles.dropdownItemStyle, ...(isSelected && {backgroundColor: '#D2D9DF'})}}>
+                <Text style={styles.dropdownItemTxtStyle}>{item.label}</Text>
+              </View>
+            );
+          }}
+          showsVerticalScrollIndicator={false}
+          dropdownStyle={styles.dropdownMenuStyle}
+        />
+      </ThemedView>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText>Data source:</ThemedText>
+        <SelectDropdown
+          ref={dataSourceRef}
+          data={dataSourceOptions}
+          defaultValue={settingsDataSource}
+          onSelect={(selectedItem, index) => {
+            setLocalDataSource(selectedItem.id);
+          }}
+          renderButton={(selectedItem, isOpened) => {
+            return (
+              <View style={styles.dropdownButtonStyle}>
+                <Text style={styles.dropdownButtonTxtStyle}>
+                  {(selectedItem && selectedItem.label) || 'Please select...'}
+                </Text>
+                <Text>{isOpened ? "🔼" : "🔽"}</Text>
+              </View>
+            );
+          }}
+          renderItem={(item, index, isSelected) => {
+            return (
+              <View style={{...styles.dropdownItemStyle, ...(isSelected && {backgroundColor: '#D2D9DF'})}}>
+                <Text style={styles.dropdownItemTxtStyle}>{item.label}</Text>
+              </View>
+            );
+          }}
+          showsVerticalScrollIndicator={false}
+          dropdownStyle={styles.dropdownMenuStyle}
+        />
+      </ThemedView>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText>Media structure:</ThemedText>
+        <SelectDropdown
+          ref={mediaStructureRef}
+          data={viewTypeOptions}
+          defaultValue={settingsMediaStructure}
+          onSelect={(selectedItem, index) => {
+            if (selectedItem.title) setStructureDescription(selectedItem.title);
+            setLocalMediaStructure(selectedItem.id);
+          }}
+          renderButton={(selectedItem, isOpened) => {
+            return (
+              <View style={styles.dropdownButtonStyle}>
+                <Text style={styles.dropdownButtonTxtStyle}>
+                  {(selectedItem && selectedItem.label) || 'Please select...'}
+                </Text>
+                <Text>{isOpened ? "🔼" : "🔽"}</Text>
+              </View>
+            );
+          }}
+          renderItem={(item, index, isSelected) => {
+            return (
+              <View style={{...styles.dropdownItemStyle, ...(isSelected && {backgroundColor: '#D2D9DF'})}}>
+                <Text style={styles.dropdownItemTxtStyle}>{item.label}</Text>
+              </View>
+            );
+          }}
+          showsVerticalScrollIndicator={false}
+          dropdownStyle={styles.dropdownMenuStyle}
+        />
+      </ThemedView>
+      <ThemedView>
+        <ThemedText>{structureDescription}</ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText>Interface type:</ThemedText>
+        <SelectDropdown
+          ref={viewOrientationRef}
+          data={uiTypeOptions}
+          defaultValue={settingsViewOrientation}
+          onSelect={(selectedItem, index) => {
+            setLocalViewOrientation(selectedItem.id);
+          }}
+          renderButton={(selectedItem, isOpened) => {
+            return (
+              <View style={styles.dropdownButtonStyle}>
+                <Text style={styles.dropdownButtonTxtStyle}>
+                  {(selectedItem && selectedItem.label) || 'Please select...'}
+                </Text>
+                <Text>{isOpened ? "🔼" : "🔽"}</Text>
+              </View>
+            );
+          }}
+          renderItem={(item, index, isSelected) => {
+            return (
+              <View style={{...styles.dropdownItemStyle, ...(isSelected && {backgroundColor: '#D2D9DF'})}}>
+                <Text style={styles.dropdownItemTxtStyle}>{item.label}</Text>
+              </View>
+            );
+          }}
+          showsVerticalScrollIndicator={false}
+          dropdownStyle={styles.dropdownMenuStyle}
+        />
+      </ThemedView>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText>UI scale:</ThemedText>
+        <Slider
+          style={{width: 200, height: 40}}
+          minimumValue={1}
+          maximumValue={10}
+          step={1}
+          value={11-settingsViewScale}
+          onSlidingComplete={handleUIScaleChange}
+          minimumTrackTintColor="#FFFFFF"
+          maximumTrackTintColor="#000000"
+        />
+      </ThemedView>
       <ThemedView style={styles.titleContainer}>
         <Button
             title="Save"
@@ -94,5 +294,50 @@ const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'row',
     gap: 8,
+  },
+  dropdownButtonStyle: {
+    width: 200,
+    height: 50,
+    backgroundColor: '#E9ECEF',
+    borderRadius: 12,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 12,
+  },
+  dropdownButtonTxtStyle: {
+    flex: 1,
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#151E26',
+  },
+  dropdownButtonArrowStyle: {
+    fontSize: 28,
+  },
+  dropdownButtonIconStyle: {
+    fontSize: 28,
+    marginRight: 8,
+  },
+  dropdownMenuStyle: {
+    backgroundColor: '#E9ECEF',
+    borderRadius: 8,
+  },
+  dropdownItemStyle: {
+    width: '100%',
+    flexDirection: 'row',
+    paddingHorizontal: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 8,
+  },
+  dropdownItemTxtStyle: {
+    flex: 1,
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#151E26',
+  },
+  dropdownItemIconStyle: {
+    fontSize: 28,
+    marginRight: 8,
   },
 });
--- store/settingsReducer.ts
+++ store/settingsReducer.ts
@@ -1,15 +1,20 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import type { RootState } from './store'
 
+export type contentTypes = 'tv' | 'movie';
+export type dataSources = 'tvdb';
+export type viewTypes = 'flat' | 'show' | 'show+season' | 'show/season';
+export type viewOrientations = 'poster' | 'banner';
+
 // Define a type for the slice state
 interface SettingsState {
   settingsPassword: string | null
   directory: string
-  contentType: 'tv' | 'movie'
-  dataSource: 'tvdb'
-  viewType: 'flat' | 'show+season' | 'show/season'
+  contentType: contentTypes
+  dataSource: dataSources
+  viewType: viewTypes
   viewScale: number
-  viewOrientation: 'poster' | 'banner'
+  viewOrientation: viewOrientations
 }
 
 // Define the initial state using that type
@@ -19,7 +24,7 @@ const initialState: SettingsState = {
   contentType: 'tv',
   dataSource: 'tvdb',
   viewType: 'show/season',
-  viewScale: 50,
+  viewScale: 5,
   viewOrientation: 'poster'
 }
 
@@ -30,18 +35,38 @@ export const settingsSlice = createSlice({
   reducers: {
     // Use the PayloadAction type to declare the contents of `action.payload`
     setDirectory: (state, action: PayloadAction<string>) => {
-      state.directory = action.payload
+      state.directory = action.payload;
     },
     setPassword: (state, action: PayloadAction<string>) => {
-      state.settingsPassword = action.payload
+      state.settingsPassword = action.payload;
+    },
+    setMediaType: (state, action: PayloadAction<contentTypes>) => {
+      state.contentType = action.payload;
+    },
+    setDataSource: (state, action: PayloadAction<dataSources>) => {
+      state.dataSource = action.payload;
+    },
+    setMediaStructure: (state, action: PayloadAction<viewTypes>) => {
+      state.viewType = action.payload;
+    },
+    setViewOrientation: (state, action: PayloadAction<viewOrientations>) => {
+      state.viewOrientation = action.payload;
+    },
+    setViewScale: (state, action: PayloadAction<number>) => {
+      state.viewScale = action.payload;
     },
   },
 })
 
-export const { setDirectory, setPassword } = settingsSlice.actions
+export const { setDirectory, setPassword, setMediaType, setDataSource, setMediaStructure, setViewOrientation, setViewScale } = settingsSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
-export const selectDirectory = (state: RootState) => state.settingsReducer.directory
-export const selectPassword = (state: RootState) => state.settingsReducer.settingsPassword
+export const selectDirectory = (state: RootState) => state.settingsReducer.directory;
+export const selectPassword = (state: RootState) => state.settingsReducer.settingsPassword;
+export const selectMediaType = (state: RootState) => state.settingsReducer.contentType;
+export const selectDataSource = (state: RootState) => state.settingsReducer.dataSource;
+export const selectMediaStructure = (state: RootState) => state.settingsReducer.viewType;
+export const selectViewOrientation = (state: RootState) => state.settingsReducer.viewOrientation;
+export const selectViewScale = (state: RootState) => state.settingsReducer.viewScale;
 
 export default settingsSlice.reducer
