8b2177b254770d0c0e7964f9dca87c8ecf85c39a
Kallum Burgin <Kallb123@users.noreply.github.com>
2024-08-18 23:06:56 +0100

    Set up app to read and list folder contents, with links

--- app/(drawer)/index.tsx
+++ app/(drawer)/index.tsx
@@ -1,23 +1,101 @@
-import { Image, StyleSheet } from 'react-native';
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { StyleSheet, View } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { Link } from 'expo-router';
+import { Href, Link } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectDirectory, selectPassword } from '@/store/settingsReducer';
+import { FlashList } from '@shopify/flash-list';
+import { FileInfo, getInfoAsync, StorageAccessFramework } from 'expo-file-system';
+
+interface MediaObject {
+  filename: string
+  path: string
+  parsedPath: string
+  isDirectory: boolean
+}
 
 export default function HomeScreen() {
+  const directory = useSelector(selectDirectory);
+  const settingsPassword = useSelector(selectPassword);
+
+  const [mediaList, setMediaList] = useState([] as MediaObject[]);
+
+  useEffect(() => {
+    // Check whether there's a directory to read
+    if (!directory) {
+      return;
+    }
+
+    checkMediaContents();
+  }, [directory]);
+
+  const checkMediaContents = async () => {
+    const contents = await StorageAccessFramework.readDirectoryAsync(directory);
+
+    var contentInfo = await Promise.all(contents.map(async (c) => {
+        try {
+          const info = await getInfoAsync(c);
+          return info;
+        } catch (e) {
+
+        }
+        const info: FileInfo = {uri: c, isDirectory: true, exists: true, size: 0, modificationTime: 0};
+        return info;
+    }));
+
+    const allContents: MediaObject[] = contentInfo.map((c) => {
+      const uri = decodeURIComponent(c.uri);
+      const filename = uri.substring(uri.lastIndexOf('/') + 1, uri.length)
+      return {
+        filename: filename,
+        path: c.uri,
+        parsedPath: uri,
+        isDirectory: c.isDirectory
+      }
+    });
+
+    const filtered = allContents.filter((c) => {
+      if (c.filename.charAt(0) === '.') return null;
+      return c;
+    })
+
+    setMediaList(filtered);
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <View>
+      {directory && mediaList && mediaList.length > 0 ? (
+      <ThemedView style={styles.listContainer}>
+        <ThemedText type="title">Current Folder</ThemedText>
+        <FlashList
+          data={mediaList}
+          renderItem={({ item }) => {
+            return (
+              <ThemedText><Link href={item.path as Href}>{item.filename}</Link></ThemedText>
+            )
+          }}
+          estimatedItemSize={200}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">SimpleMediaBrowser!</ThemedText>
-        <HelloWave />
+      </ThemedView>
+      ) : (
+        <ThemedView style={styles.stepContainer}>
+          <ThemedText type="subtitle">Problem</ThemedText>
+          {
+            directory ? (
+              <ThemedText>
+                Your library directory is empty or invalid, set it up in <Link href={settingsPassword ? "/(drawer)/settingsprompt" : "/settings"}>Settings</Link>.
+              </ThemedText>
+            ) : (
+              <ThemedText>
+                You need to set up a library directory in <Link href={settingsPassword ? "/(drawer)/settingsprompt" : "/settings"}>Settings</Link>.
+              </ThemedText>
+            )
+          }
+        </ThemedView>
+      )}
+      {/* <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">SimpleMediaBrowser Home</ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
@@ -40,8 +118,8 @@ export default function HomeScreen() {
           <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      </ThemedView> */}
+    </View>
   );
 }
 
@@ -54,6 +132,9 @@ const styles = StyleSheet.create({
   stepContainer: {
     gap: 8,
     marginBottom: 8,
+  },
+  listContainer: {
+    height: "100%",
   },
   reactLogo: {
     height: 178,
